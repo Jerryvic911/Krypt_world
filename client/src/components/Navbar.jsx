4a51681acb3bc5bd09022c9ac9c28ca0b9e98593
jeremiahv <jeremiahv791@gmail.com>
2024-12-14 17:23:18 +0100

    name changed, icon changed and sidebar fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,9 +15,10 @@ const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-   <nav className="w-full flex md:justify-center justify-between items-center p-4">
-    <div className="md:flex-[0.5] flex-initial justify-center items-center">
-      <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+   <nav className="w-full flex md:justify-center justify-between items-center p-3 gap-10">
+    <div className="md:flex-[0.5] flex justify-center items-center">
+      <img src="https://img.icons8.com/?size=100&id=6FfkEgdcnZ3y&format=png&color=000000" alt="logo" className="w-32 h-20 object-contain cursor-pointer" />
+      <h1 className="text-white pr-12 font-bold text-3xl md:text-4xl">KRYPTO</h1>
     </div>
 
     <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
@@ -33,7 +34,8 @@ const Navbar = () => {
            : <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)}/>
           }
            {toggleMenu && (
-            <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:'hidden list-none flex-col justify-start items-end bg-white rounded-md animate-slide-in">
+            <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+            flex flex-col justify-start items-center rounded-md blue-glassmorphism text-white animate-slide-in">
             <li className="text-xl w-full my-2 cursor-pointer">
             <AiOutlineClose onClick={() => setToggleMenu(false)}/>
             </li>
